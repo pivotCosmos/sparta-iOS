6c7329a6587f1a89812436d61d4cf193280710dd
Jeongah <adeemgin@naver.com>
2022-09-01 12:00:14 +0900

    손가락묶기 게임 - 터치 메소드 4가지, 멀티터치

---
+++ week05/FingerGame/FingerGame/FingerGameView.swift
@@ -0,0 +1,51 @@
+//
+//  FingerGameView.swift
+//  FingerGame
+//
+//  Created by 이정아 on 2022/09/01.
+//
+
+import UIKit
+
+class FingerGameView: UIView {
+
+    var touchToRoundView: [UITouch: UIView] = [:]
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            
+            let view = UIView(frame: CGRect(x: 0, y: 0, width: 80, height: 80)) //모서리에 위치한 정사각형
+            view.center = touch.location(in: self) //x,y(정사각형 위치)가 알아서 바뀐다
+            view.backgroundColor = UIColor(named: "Strawberry")
+            view.layer.cornerRadius = 40 //80x80 정사각형을 반씩 깎아서 원으로 만든다
+            
+            self.touchToRoundView[touch] = view
+            self.addSubview(view)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let view = self.touchToRoundView[touch]
+            view?.center = touch.location(in: self)
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let view = self.touchToRoundView[touch]
+            
+            view?.removeFromSuperview()
+            self.touchToRoundView.removeValue(forKey: touch)
+        }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let view = self.touchToRoundView[touch]
+            
+            view?.removeFromSuperview()
+            self.touchToRoundView.removeValue(forKey: touch)
+        }
+    }
+}
---
+++ week05/FingerGame/FingerGame/FingerGameViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FingerGameViewController.swift
+//  FingerGame
+//
+//  Created by 이정아 on 2022/09/01.
+//
+
+import UIKit
+
+class FingerGameViewController: UIViewController {
+
+    @IBOutlet weak var gameView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButtonClicked(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
